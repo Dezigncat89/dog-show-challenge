884d89ef4df82c806dc4daeb7729051630a11c38
Elaina Sorenson <78282104+Dezigncat89@users.noreply.github.com>
2021-04-20 08:49:27 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
 
 })
-
+//Fetch Request to Render
 async function getdogs() {
     let url = 'dogs.json';
     try {
@@ -17,9 +17,10 @@ async function renderDogs() {
     Dogs.forEach(Dog => {
         let htmlSegment = `<div class="table">
     <h2>${table.name}</h2>
-    <h3>${table.Dog Breed
+    <h3>${table.breed}</h3>
+    <h4>${table.sex}</h4>
     <p>4 Likes </p>
-    <button class="like-btn" id="[toy_id]">Like <3</button>
+    <button class="like-btn" id="[dog_id]">Like <3</button>
   </div> 
     </div>`;
 
@@ -28,4 +29,31 @@ async function renderDogs() {
 
     let container = document.querySelector('.container');
     container.innerHTML = html;
+}
+
+//Edit Dogs Info on Table
+dogContainer.addEventListener("submit", function (e) {
+e.preventDefault()
+editDogs(e.target, e.target.dataset.formId)
+})
+function editDogs(name, breed, sex) {
+fetch(`http://localhost:3000/dogs/${dogId}`, {
+method: "PATCH",
+headers: {
+"Content-type": "application/json",
+"accept": "application/json"
+},
+body: JSON.stringify({
+name: nameInfo.name.value
+breed: breedInfo.breed.value
+sex: sexInfo.sex.value
+})
+})
+.then(res => res.json())
+.then( dog => {
+fetchDogs()
+nameInfo.reset()
+breedInfo.reset()
+sexInfo.reset()
+})
 }
