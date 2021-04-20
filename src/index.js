cc820661ff6f06433af4ce5f7b75fceb7cba6df1
Elaina Sorenson <78282104+Dezigncat89@users.noreply.github.com>
2021-04-20 08:25:09 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 document.addEventListener('DOMContentLoaded', () => {
 
 })
+
+async function getdogs() {
+    let url = 'dogs.json';
+    try {
+        let res = await fetch(url);
+        return await res.json();
+    } catch (error) {
+        console.log(error);
+    }
+}
+async function renderDogs() {
+    let Dogss = await getDogs();
+    let html = '';
+    Dogs.forEach(Dog => {
+        let htmlSegment = `<div class="table">
+    <h2>${table.name}</h2>
+    <h3>${table.Dog Breed
+    <p>4 Likes </p>
+    <button class="like-btn" id="[toy_id]">Like <3</button>
+  </div> 
+    </div>`;
+
+        html += htmlSegment;
+    });
+
+    let container = document.querySelector('.container');
+    container.innerHTML = html;
+}
